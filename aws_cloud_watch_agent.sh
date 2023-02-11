da492a103bfda150044ff3079746ab7ea58fcf02
srijansankalp <121816200+srijansankalp@users.noreply.github.com>
2023-02-11 14:32:42 -0500

    Add files via upload

---
+++ aws_cloud_watch_agent.sh
@@ -0,0 +1,17 @@
+#install the agent on AmzaonLinux2
+sudo yum install amazon-cloudwatch-agent
+
+#run the wizard
+sudo /opt/aws/amzaon-cloudwatch-agent/bin/amazon-cloudwatch-agent-config-wizard
+
+#create some missing files
+sudo mkdir -p /usr/share/collectd
+sudo touch /usr/share/collectd/types.db
+
+#options:
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-linux -s
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+
+
